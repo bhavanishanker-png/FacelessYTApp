c7b738494a6fab6ac4d3732d55185c86c82e2a80
bhavanishanker <bhavanishankerpichuka@gmail.com>
2026-04-13 09:09:07 +0530

    remove all project files and source code

--- ai-youtube-saas/app/project/[id]/loading.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { ProjectWorkspaceSkeleton } from "@/components/ProjectWorkspaceSkeleton";
-
-export default function LoadingProject() {
-  return <ProjectWorkspaceSkeleton />;
-}
--- ai-youtube-saas/testdb.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Project = require("./models/Project").default || require("./models/Project");
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://developer:developer123@velora.yab0h.mongodb.net/velora?retryWrites=true&w=majority")
-  .then(async () => {
-    const p = await Project.findOne({});
-    console.log("Found project currentStep:", p.currentStep);
-    process.exit(0);
-  });
